7191adde6baeabef09143f48b9abb380c8e0fa48
Cristian727 <cristian.gonzalez7@hotmail.com>
2024-11-04 11:11:24 +0100

    fix code

--- server.js
+++ server.js
@@ -5,19 +5,26 @@ const app = express()
 const port = 3000
 
 //const comando = 'mkdir carpetaRandom';
-const comando = "figlet hola mundo"
+//const comando = "figlet hola mundo"
 //const comando = 'echo "Hola mundo" > Test.txt'
 
 
-app.get("/", (req, res) => {
+app.use(express.static("public"))
+
+app.get("/", (req,res) => {
     exec(comando, (error, stdout, stderr) => {
-    //console.log(error, stdout, stderr)
-    console.log(`Stdout: ${stdout}`)
-    res.send(`<pre>${stdout}</pre>`)
-    })
+        res.send(`<pre>${stdout}</pre>`)
+      });
 })
 
+app.get("/ping", (req, res) => {
+    const dominio = req.query.dominio
+    const comando = "ping -c 4 " + dominio
+    exec(comando, (error, stdout, stderr) => {
+        res.send(stdout)
+    });
+})
 
 app.listen(port, () => {
-    console.log('Servidor escuchando en el puerto 3000');
+    console.log(`Servidor funcionando en ${port}`)
 })
