60e0184c52adbd714b5ac06afd218203831ab12d
Cristian727 <cristian.gonzalez7@hotmail.com>
2024-11-04 10:15:17 +0100

    Pruebas

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require("express")
+const { exec } = require('child_process')
+//const comando = 'ping -c 4 google.com'
 const app = express()
 const port = 3000
+
+//const comando = 'mkdir carpetaRandom';
+const comando = "figlet hola mundo"
+//const comando = 'echo "Hola mundo" > Test.txt'
+
+
 app.get("/", (req, res) => {
-    res.send("Hola mundo")
+    exec(comando, (error, stdout, stderr) => {
+    //console.log(error, stdout, stderr)
+    console.log(`Stdout: ${stdout}`)
+    res.send(`<pre>${stdout}</pre>`)
+    })
 })
 
-app.listen(3000, () => {
+
+app.listen(port, () => {
     console.log('Servidor escuchando en el puerto 3000');
 })
